a1e2f4de3e5233cfd0475681652f5a5090e9020b
Punnen <npunnen@amazon.com>
2022-11-16 23:12:09 -0800

    Amplify changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,26 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "artifactstoreb96ea463": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "uploadFiles": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "landingZone": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
---
+++ src/graphql/queries.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getArtifact = /* GraphQL */ `
+  query GetArtifact($id: ID!) {
+    getArtifact(id: $id) {
+      id
+      name
+      description
+      createdBy
+      department
+      fileUrl
+      fileName
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listArtifacts = /* GraphQL */ `
+  query ListArtifacts(
+    $filter: ModelArtifactFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listArtifacts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        createdBy
+        department
+        fileUrl
+        fileName
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
